bee019c768c855dd6fcb4a95cd0f5669a6bea5c0
Vinicius Vieira <vini.clau0@gmail.com>
2025-09-17 21:54:11 -0300

    feat: register page

--- frontend/vue-project/src/router/index.js
+++ frontend/vue-project/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
+import Register from '@/views/Register.vue'
 
 const routes = [
   {
@@ -10,6 +11,13 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
   }
 ]
 
---
+++ frontend/vue-project/src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Cadastro</h2>
+    <form>
+      <div>
+        <label for="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          required
+          placeholder="Seu nome completo"
+        />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          required
+          placeholder="Seu email"
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          required
+          placeholder="Sua senha"
+          minlength="6"
+        />
+      </div>
+      <button type="submit">
+      </button>
+      <p>
+        Já tem uma conta? <router-link to="/login">Faça login</router-link>
+      </p>
+    </form>
+  </div>
+</template>
